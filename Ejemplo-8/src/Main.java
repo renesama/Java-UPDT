02df6aa33e2b6130478daa8f2748f3a8d161700d
rene ulises <rene_ulises@me.com>
2018-06-29 09:57:34 -0500

    Se agrego arreglo persona que guarda estudiantes y trabajadores

--- Ejemplo-8/src/Main.java
+++ Ejemplo-8/src/Main.java
@@ -21,7 +21,7 @@ public class Main {
         listaEstudiantes.get(2).setGrupo("C");
         listaEstudiantes.get(2).setMatricula("lgm20183");
         listaTrabajadores.add(new Trabajador("alfredo", "santana","mercado"));
-        listaTrabajadores.add(new Trabajador("David","huerta","rodriguez"));
+        listaTrabajadores.add(new Trabajador("david","huerta","rodriguez"));
         listaTrabajadores.add(new Trabajador("santiago", "acosta", "vera"));
         listaTrabajadores.get(0).setRfc("asdf123");
         listaTrabajadores.get(0).setSueldo("16000");
@@ -36,8 +36,43 @@ public class Main {
         for (Trabajador trab:listaTrabajadores){
             listaPersonas.add(trab);
         }
+
+
+        Estudiante[] arrayAlumnos = new Estudiante[3];
+        Trabajador[] arrayTrabajadores = new Trabajador[3];
+        Persona[] arrayPersona = new Persona[6];
+
+        arrayAlumnos[0]= new Estudiante("luis", "gomez", "lopez");
+        arrayAlumnos[1]= new Estudiante("jose", "robles", "prieto");
+        arrayAlumnos[2]= new Estudiante("mario","solis","luna");
+        arrayAlumnos[0].setGrado("primero");
+        arrayAlumnos[0].setGrupo("A");
+        arrayAlumnos[0].setMatricula("lgm20181");
+        arrayAlumnos[1].setGrado("segundo");
+        arrayAlumnos[1].setGrupo("B");
+        arrayAlumnos[0].setMatricula("jrp20182");
+        arrayAlumnos[2].setGrado("tercero");
+        arrayAlumnos[2].setGrupo("C");
+        arrayAlumnos[0].setMatricula("lgm20183");
+
+        arrayTrabajadores[0] = new Trabajador("alfredo", "santana","mercado");
+        arrayTrabajadores[1] = new Trabajador("david","huerta","rodriguez");
+        arrayTrabajadores[2] = new Trabajador("santiago", "acosta", "vera");
+        arrayTrabajadores[0].setSueldo("16000");
+        arrayTrabajadores[1].setSueldo("17000");
+        arrayTrabajadores[2].setSueldo("18000");
+        arrayTrabajadores[0].setRfc("asdf123");
+        arrayTrabajadores[1].setRfc("qwer133");
+        arrayTrabajadores[2].setRfc("zxcv143");
+
+        for(int i=0; i<3;i++){
+            arrayPersona[i] = arrayAlumnos[i];
+        }
         System.out.println();
+        for(int i=0; i<3;i++){
+            arrayPersona[i+3] = arrayTrabajadores[i];
+        }
 
-
+        System.out.println();
     }
 }
