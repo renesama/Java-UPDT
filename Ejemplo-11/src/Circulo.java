7b69d2418ebf1e325b533a1a31d5d5c03877083a
rene ulises <rene_ulises@me.com>
2018-07-24 16:20:15 -0500

    Clases Abstractas

---
+++ Ejemplo-11/src/Circulo.java
@@ -0,0 +1,13 @@
+public class Circulo extends Figura
+{
+    private double radio;
+    public Circulo (double x, double y, double radio)
+    {
+        super(x,y);
+        this.radio = radio;
+    }
+    public double area ()
+    {
+        return Math.PI*radio*radio;
+    }
+}
---
+++ Ejemplo-11/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Cuadrado uno = new Cuadrado(12,23,23);
+        Circulo dos = new Circulo(12,12,13);
+        //Figura tres = new figura(12,12);
+        System.out.println("Hello World!");
+    }
+}
